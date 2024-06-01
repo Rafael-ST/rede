53ccae759d41d0b3fc32c0ed2fb9e9e68dd309f4
Rafael Santos <rafaelsantost87@gmail.com>
2024-06-01 15:46:35 -0300

    bairros

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 from .models import Bairro, LiderDeEquipe, Amigo
+from import_export import resources, fields
+from import_export.admin import ImportExportModelAdmin, ExportMixin
 
-admin.site.register(Bairro)
+class BairroResource(resources.ModelResource):
+    class Meta:
+        model = Bairro
+        fields = ('nome',)
+        import_id_fields = []
+
+
+class BairroAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    resource_class = BairroResource
+
+admin.site.register(Bairro, BairroAdmin)
 admin.site.register(LiderDeEquipe)
 admin.site.register(Amigo)
--- rede/settings.py
+++ rede/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'crispy_forms',
     'crispy_bootstrap5',
+    'import_export',
 ]
 
 
@@ -105,23 +106,23 @@ SIMPLE_JWT = {
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mydb',
-        'USER': 'admin',
-        'PASSWORD': '123987',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'mydb',
+#         'USER': 'admin',
+#         'PASSWORD': '123987',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
 
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
