f621c0ae279e86dab0594a81944e0084bf119a81
Rafael Santos <rafaelsantost87@gmail.com>
2024-06-01 14:48:07 -0300

    debug false

--- rede/settings.py
+++ rede/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dct3p)-5!aneq^e4l9zglc@y3$jj=c95r-xx63*1hfq_9h3fwe'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['35.198.58.96', '35.198.58.96:8000']
 
